8dbbf9350f688c2c3595d14237210700f3053800
Dominik Szarkiel <d.szarkiel@gmail.com>
2020-10-05 20:14:43 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,5 +1,7 @@
 def find_element_index(array, value_to_find)
-  until array.include?(value_to_find) do
-    array.each 
+  if array.include?(value_to_find)
+    puts value_to_find
+  else
+    nil
   end
 end
